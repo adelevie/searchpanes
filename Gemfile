5583bc1df89008957d58e87ea67193ce61ced1d7
Alan deLevie <adelevie@gmail.com>
2012-03-01 15:50:53 -0500

    imports fcc stylesheets for left pane

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :rubygems
 
 gem "sinatra", :require => "sinatra/base"
+gem "nokogiri"
--- main.rb
+++ main.rb
@@ -1,19 +1,39 @@
 require 'open-uri'
 require 'uri'
+require 'nokogiri'
 
 class App < Sinatra::Base
   get "/" do
     erb :index
   end
   
+  def insert_absolute_path(url)
+    html = open(url).read
+    doc = Nokogiri::HTML html
+    links = doc.xpath("html/head/link")
+    links.each do |link|
+      link.attributes["href"].value = "http://fcc.gov" + link.attributes["href"].value
+    end
+    
+    doc
+  end
+  
   get "/fcc/:term" do
     url = "http://www.fcc.gov/search/results/" + URI.encode(params[:term])
-    open(url).read
+    #open(url).read
+    doc = insert_absolute_path url
+    doc.to_s
   end
   
   get "/searchusa/:term" do
     url = "https://search.usa.gov/search?&affiliate=fcc&query=" + URI.encode(params[:term])
     open(url).read
+  end
+  
+  get "/foo" do
+    url = "http://www.fcc.gov/search/results/" + URI.encode(params[:term])
+    #open(url).read
+    doc = insert_absolute_path url
   end
 end
 
