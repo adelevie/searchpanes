4b960e45d6e379fd2c7c9214defdb6fb167f0620
Alan deLevie <adelevie@gmail.com>
2012-03-15 00:37:27 -0400

    black arts

--- main.rb
+++ main.rb
@@ -3,31 +3,40 @@ require 'uri'
 require 'nokogiri'
 
 class App < Sinatra::Base
-  get "/" do
-    erb :index
-  end
   
   def insert_absolute_path(url, base)
     html = open(url).read
     doc = Nokogiri::HTML html
-    links = doc.xpath("html/head/link")
-    links.each do |link|
-      link.attributes["href"].value = base + link.attributes["href"].value
+    doc.xpath("html/head/link").each do |link|
+      uri = link.attributes["href"]
+      uri.value = base + uri.value
     end
     
-    doc
+    str1 = "/sites/all/themes/fcc/images/logoFCC1.jpg"
+    rplc1 = base + str1
+    
+    str2 = "/sites/all/themes/fcc/images/logoFCC2.jpg"
+    rplc2 = base + str2
+    
+    doc = doc.to_s.gsub(str1, rplc1)
+    doc.gsub(str2, rplc2)
+  end
+  
+  get "/" do
+    erb :index
   end
   
   get "/fcc/:term" do
     url = "http://www.fcc.gov/search/results/" + URI.encode(params[:term])
     doc = insert_absolute_path(url, "http://fcc.gov")
-    doc.to_s
+    
+    doc
   end
   
   get "/searchusa/:term" do
     url = "https://search.usa.gov/search?&affiliate=fcc&query=" + URI.encode(params[:term])
     doc = insert_absolute_path(url, "https://search.usa.gov")
-    doc.to_s
+    doc
   end
-end
 
+end
